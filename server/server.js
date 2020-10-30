87c29d9929873979d3668c5123f959dbf5bc5eb2
Stanley <staanleyminervini@gmail.com>
2020-10-29 22:59:42 -0400

    Update Access Control

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ const app = express();
 app.use(bodyparser.json());
 app.use(express.static(`static`));
 
+app.use((req, res, next) => {
+  res.setHeader(`Access-Control-Allow-Origin`, `*`);
+  res.setHeader(
+    `Access-Control-Allow-Headers`,
+    `Origin, X-Requested-With, Content-Type, Accept, Authorization`
+  );
+  res.setHeader(
+    `Access-Control-Allow-Methods`,
+    `GET, POST, PATCH, DELETE, OPTIONS`
+  );
+  next();
+});
+
 app.post(
   `/api/login`,
   wrapAsync(async (req, res) => {
