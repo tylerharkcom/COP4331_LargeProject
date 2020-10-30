f0bb7f0684e321fbb0d2867d3f14cb3313ac0871
StaceyD22 <stacey.dale2311@gmail.com>
2020-10-29 20:11:57 -0400

    Update frontend

---
+++ client/src/pages/DashboardPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import UserNavBar from '../components/UserNavBar';
+import UserDashboard from '../components/UserDashboard';
+
+const DashboardPage = () =>
+{
+
+    return(
+      <div id="dashboardPage">
+        <UserNavBar />
+      </div>
+    );
+};
+
+export default DashboardPage;
