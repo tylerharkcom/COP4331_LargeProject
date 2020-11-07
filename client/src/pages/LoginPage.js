e25e7deaca9e56ae7dbbb85f259fb668f2f03eda
Clev7 <49847280+Clev7@users.noreply.github.com>
2020-11-07 16:38:03 -0500

    Revert "Sorry"
    This reverts commit 011530ce9254f7af4166b62fa60bc8a63d827d10.

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Login from '../components/Login';
+
+const LoginPage = () =>
+{
+
+    return(
+      <div id="loginPage">
+        <Login />
+      </div>
+    );
+};
+
+export default LoginPage;
