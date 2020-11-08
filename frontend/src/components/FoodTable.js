e836fedeadf6d84fbfb78b73ac8b753a769613bb
StaceyD22 <stacey.dale2311@gmail.com>
2020-11-08 12:18:54 -0500

    Updated stuff

--- frontend/src/components/FoodTable.js
+++ frontend/src/components/FoodTable.js
@@ -15,6 +15,11 @@ function FoodTable() {
         alert('Thought you could add food, did ya?');
     }
 
+    const deleteFood = event => {
+        event.preventDefault();
+        alert('Thought you could delete food, did ya?');
+    }
+
     return(
         <div id="fridgeTable">
             <div id="fridgeCRUD">
@@ -22,11 +27,22 @@ function FoodTable() {
                     className="btn btn-secondary"
                     onClick={addFood}
                 >Add food</button>
+                <button
+                    className="btn btn-secondary"
+                    onClick={deleteFood}
+                    style={{paddingLeft: "5px"}}
+                >Delete</button>
             </div>
             <Table striped bordered hover variant="dark">
                 <thead>
                     <tr>
-                    <th></th>
+                    <th>
+                        <div id="selectAll" class="checkbox">
+                            <label>
+                            <input type="checkbox" /> Select All
+                            </label>
+                        </div>
+                    </th>
                     <th>Food</th>
                     <th>Date expires</th>
                     <th>Date added</th>
@@ -44,10 +60,15 @@ function FoodTable() {
                     <td>
                         <Icon
                             name = "edit"
-                            tooltip = "Edit"
                             size = "small"
                             theme = "dark"
                             onClick = {editFood}
+                        />
+                        <Icon
+                            name = "delete"
+                            size = "small"
+                            theme = "dark"
+                            onClick = {deleteFood}
                         />
                     </td>
                     </tr>
