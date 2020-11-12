a36926cd482dd5f833b9752f5cf2e373a4db979d
Jonathan Frucht <jonfrucht@gmail.com>
2020-11-12 16:08:23 -0500

    updated MyAccount.js

--- frontend/src/components/MyAccount.js
+++ frontend/src/components/MyAccount.js
@@ -1,25 +1,52 @@
-//import React from "react";
+// TODO
+// 1. Make PW hidden -- Done
+// 2. Connect UpdatePWButton to PW check func --done
+// 3. Replace Pw requirement paragraph w Tooltip --done
+// 4. Add error message for curr & new PW's -- done
+// 5. When checkbox gets clicked, Update pw appears --Hard
+// 6. Resize tooltip & style it so it looks nice
+
 import React, { useState, useEffect } from "react";
 import ListGroup from 'react-bootstrap/ListGroup'
 import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import Tooltip from 'react-bootstrap/Tooltip'
+import Popover from 'react-bootstrap/Popover'
+import Overlay from 'react-bootstrap/Overlay'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Switch from 'react-switch'
 import sha256 from '../sha256';
 
+ 
 function MyAccount() 
 {
-    // var fName;
-    // var lName;
-    // var email;
-    // var loginName; // Username?
     var newPassword1;
     var newPassword2;
     var currPassword;
+    var showSubmit; 
+    const pwRequirements = 'Your new password should: be at least 8 characters long contain at least one capital letter, at least one lowercase letter, and at least one digit contain at least one of the following special characters: @, !, #, $, %, *';
+                            
 
     const [show, setShow] = useState(false);
-    const [message,setMessage] = useState('');
+   // const [showSubmit,handleChange] = useToggle();
+    const [messageCurr,setMessageCurr] = useState('');
+    const [messageNewPW,setMessageNewPW] = useState('');
 
+    const renderTooltip = (props) => (
+        <Tooltip id="button-tooltip" style={{width: "150%", }} {...props}>
+           Your new password should:
+                                <ul>
+                                    <li>be at least 8 characters long</li>
+                                    <li>contain at least one capital letter, at least one lowercase letter, and at least one digit</li>
+                                    <li>contain at least one of the following special characters: @, !, #, $, %, *</li>
+                                </ul>
+        </Tooltip>
+      );
+    // const handleChange = () => {
+    //     setShowUpdate()
+    // };
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     
@@ -37,68 +64,69 @@ function MyAccount()
     });
 
     const refreshPage = async (event) => {
-        alert('Theres always going to be another mountain');
+        alert('Theres always going to be another mountain'); //filler
        //  window.location.reload(false);  // Deprecated 
        // Maybe create an updateField
     }
     
     const updatePassword = async (event) => {
         //Api call to update PW
-        alert('I think i thought i saw you try')
+        alert('Kevins Famous Chili'); // filler
     }
     // Launched from the Modal, find out how to launch from button click
     const updatePasswordCheck =  async (event) => {
-        var pwCheck = false; 
         event.preventDefault();
-        alert('Im always going to want to make it move');
         // Check if new password meets criteria
-        if(!pwCheck){
-            if(newPassword1.value==="")
-            {
-                setMessage('A password is required');
-                return;
-            }
-
-            var expression = /^\w+$/;
-
-            if(newPassword1.value.length<8)
-            {
-                setMessage('Your new password must be at least 8 characters long');
-                return;
-            }
-
-            expression = /[!@#$%*]/;
-            if(!expression.test(newPassword1.value))
-            {
-                setMessage('Your new password must contain at least one of the following special character: @, !, #, $, %, *');
-                return;
-            }
-
-            expression = /[0-9]/
-            if(!expression.test(newPassword1.value))
-            {
-                setMessage('Your new password must contain at least one digit (0-9)');
-                return;
-            }
-
-            expression = /[a-z]/
-            if(!expression.test(newPassword1.value))
-            {
-                setMessage('Your new password must contain at least one lowercase letter');
-                return;
-            }
-
-            expression = /[A-Z]/
-            if(!expression.test(newPassword1.value))
-            {
-                setMessage('Your new password must contain at least one uppercase letter');
-                return;
-            }
+        if(currPassword.value==="")
+        {
+            setMessageCurr('Your current password is required');
+            return;
         }
+        
+        if(newPassword1.value==="")
+        {
+            setMessageNewPW('A new password is required');
+            return;
+        }
+
+        var expression = /^\w+$/;
+        if(newPassword1.value.length<8)
+        {
+            setMessageNewPW('Your new password must be at least 8 characters long');
+            return;
+        }
+
+        expression = /[!@#$%*]/;
+        if(!expression.test(newPassword1.value))
+        {
+            setMessageNewPW('Your new password must contain at least one of the following special character: @, !, #, $, %, *');
+            return;
+        }
+
+        expression = /[0-9]/
+        if(!expression.test(newPassword1.value))
+        {
+            setMessageNewPW('Your new password must contain at least one digit (0-9)');
+            return;
+        }
+
+        expression = /[a-z]/
+        if(!expression.test(newPassword1.value))
+        {
+            setMessageNewPW('Your new password must contain at least one lowercase letter');
+            return;
+        }
+        expression = /[A-Z]/
+        if(!expression.test(newPassword1.value))
+        {
+            setMessageNewPW('Your new password must contain at least one uppercase letter');
+            return;
+        }
+        
         // Check if both new password fields are equal
         if (!(newPassword1.value == newPassword2.value))
         {
-            setMessage('Your new passwords do not match');
+            setMessageNewPW('Your new passwords do not match');
             return;
         }
         // Check if current password is correct
@@ -116,12 +144,12 @@ function MyAccount()
             var res = JSON.parse(await response.text());
       
             if (response.status !== 200) {
-              setMessage(res.error);
+              setMessageNewPW(res.error);
             } else {
               updatePassword();
       
-              setMessage("Password successfully updated!");
-              window.location.href = "/MyAccount";
+              setMessageNewPW("Password successfully updated!");
+              window.location.href = "/account";
             }
           } catch (e) {
             alert(e.toString());
@@ -129,42 +157,6 @@ function MyAccount()
           }
         
     }
-    // Func to load in data as page is entered. May be redudant if pulled from cache
-    // Not up to date
-    /*
-    const getAccountinfo = async (event) => // Find what kind of event this requires
-    {
-        var obj = {
-            firstName: firstName.value, 
-            lastName: lastName.value, 
-            email: email.value,
-            user: loginName.value
-        };
-        var js = JSON.stringify(obj);
-        try {
-            const response = await fetch("http://localhost:5000/api/accountInfo",
-            {
-             method: "POST",
-             body: js, 
-             headers : { "Content-Type": "application/json" }, 
-            });
-
-            var res = JSON.parse(await response.text());
-
-            if (res.id <= 0){} // Dev msg for debugging
-               // setMessage("Error: Information not retrieved from the database");
-            else
-            {
-               // var user = {firstName: res.fName, lastName: res.lName, email: res.email};
-                // do something with data
-            }
-        } catch (e) 
-        {
-            alert(e.toString());
-            return;
-        }
-    };
-    */
 
     return (
         <div id="accountDiv" class= "center">
@@ -194,39 +186,36 @@ function MyAccount()
                             <Form.Group controlId="currentPassword">
                                 <Form.Label>Current password</Form.Label>
                                 <Form.Control 
-                                    type="" 
+                                    type="password" 
                                     placeholder="Current password"
                                     ref={(c) => (currPassword = c)} 
                                 />
                                 <Form.Text className="text-muted">
                                 </Form.Text>
-                            </Form.Group>
+                                <span id="currPasswordMSG">{messageCurr}</span>
+                            </Form.Group>                        
                             <Form.Label htmlFor="newPassword1">New Password</Form.Label>
+                            <OverlayTrigger
+                             placement="bottom"
+                             overlay={renderTooltip}
+                            >
                             <Form.Control
                                 type="password"
                                 id="newPassword1"
-                                aria-describedby="passwordHelpBlock"
+                                aria-describedby= {pwRequirements}
                                 ref={(c) => (newPassword1 = c)}     
                             />
-                            <Form.Text id="passwordHelpBlock" muted>
-                                Your new password should:
-                                <ul>
-                                    <li>be at least 8 characters long</li>
-                                    <li>contain at least one capital letter, at least one lowercase letter, and at least one digit</li>
-                                    <li>contain at least one of the following special characters: @, !, #, $, %, *</li>
-                                </ul>
-                            </Form.Text>
+                            </OverlayTrigger>
                             <Form.Group controlId="newPassword2">
                                 <Form.Label>Please enter your new password again</Form.Label>
                                 <Form.Control 
-                                    type="newPW"
+                                    type="password"
                                     ref={(c) => (newPassword2 = c)}
                                 />
                             </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Confirm password change(add onclick->)"  />
-                            </Form.Group> 
-                            <Button variant="primary" type="submit"  onClick={updatePasswordCheck}> 
+                            <span id="newPasswordMSG" >{messageNewPW}</span>
+                            
+                            <Button id="UpdatePWBut" show={showSubmit} variant="primary" type="submit"  onClick={updatePasswordCheck}> 
                                 Update Password
                             </Button>
                         </Form>
@@ -244,3 +233,14 @@ function MyAccount()
 }
 
 export default MyAccount;
+//   <Form.Check type="checkbox" label="Confirm password change(4add onclick->)" onClick={handleChange} />
+// Safekeep, from line 245 ^^
+{/* <Form.Group controlId="toggleConfirm">
+                            <Toggle
+                                id = 'toggleConfirmId'
+                                defaultChecked={false}
+                                onChange={handleChange}  />
+                            <span>Confirm password change</span>
+                            
+
+                            </Form.Group>  */} // Safekeep from under newpw2
