210d99d20084afbceef5712062664a9e7bcbfdee
Stanley <staanleyminervini@gmail.com>
2020-11-25 18:30:30 -0500

    Rolling back

--- server/server.js
+++ server/server.js
@@ -429,81 +429,74 @@ router.post(
 router.post(
   "/updateAccount",
   wrapAsync(async (req, res, next) => {
-    const { username, email, fName, lName } = req.body;
+    const {
+      username,
+      email,
+      fName,
+      lName,
+      bDay,
+      gender,
+      country,
+      language,
+    } = req.body;
 
     const newInfo = {
-      username: "",
-      email: "",
-      fName: "",
-      lName: "",
+      email,
+      fName,
+      lName,
+      bDay,
+      gender,
+      country,
+      language,
     };
 
     const response = {
       error: "",
     };
 
-    if (!userename && !email && !fName && !lName) {
+    if (!username || !email || !fName || !lName) {
       response.error = "No data entered";
+      res.status(400).json(response);
       return;
     }
-
     const db = client.db();
-
-    const user = await db.collection("Users").findOne({ _id: req._id });
-
-    if (!username) {
-      newInfo.username = user.username;
+    var emailCheck, usernameCheck;
+    if (email != req.user.email) {
+      emailCheck = await db.collection("Users").findOne({ email });
     }
-
-    if (!email) {
-      newInfo.email = user.email;
+    if (username != req.user.username) {
+      usernameCheck = await db.collection("Users").findOne({ username });
     }
-
-    if (!fName) {
-      newInfo.fName = user.fName;
-    }
-
-    if (!lName) {
-      newInfo.lName = user.lName;
-    }
-
-    const emailCheck = await db.collection("Users").findOne({ email });
-    const usernameCheck = await db.collection("Users").findOne({ username });
 
     if (emailCheck) {
       response.error = "Email already taken";
       res.status(400).json(response);
       return;
     }
-
     if (usernameCheck) {
       response.error = "Username already taken";
       res.status(400).json(response);
       return;
     }
-
     if (typeof username != "string") {
       // validating data to string
       response.error = "invalid data";
       res.status(400).json(response);
       return;
     }
-    if (typeof password != "string") {
-      response.error = "invalid data";
-      res.status(400).json(response);
-      return;
-    }
-
-    // Maybe check type of fName and lName as well?
 
     try {
       await db
         .collection("Users")
-        .updateOne({ userId: user._id }, { $set: { userInfo: newInfo } });
+        .updateOne(
+          { _id: req.user._id },
+          { $set: { userInfo: newInfo, username } }
+        );
     } catch (e) {
       console.log(e);
       response.error = e;
       res.status(400).json(response);
+      return;
     }
 
     res.status(200).json(response);
