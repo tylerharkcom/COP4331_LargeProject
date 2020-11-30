610d2d4e15cb4d85eafd5525a72df630720f9856
Clev7 <49847280+Clev7@users.noreply.github.com>
2020-11-30 05:07:39 -0500

    Added ChangePassPage

---
+++ frontend/src/pages/ChangePassPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChangePass from '../components/ChangePass';
+
+const ChangePassPage = () => 
+{
+  return (
+    <div id="ChangePassPage" class="backgroundPage">
+      <ChangePass />
+    </div>
+  );
+};
+
+export default ChangePassPage;
