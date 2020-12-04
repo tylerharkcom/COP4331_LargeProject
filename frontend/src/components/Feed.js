1be3a3315b6691ae0fce7c496574f4e7482969dc
Jonathan Frucht <jonfrucht@gmail.com>
2020-12-04 15:26:16 -0500

    Testing updated Feed

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -5,7 +5,7 @@ import Placeholder from "../images/orangeSliceCrop.jpg";
 import Image from "react-bootstrap/Image";
 import React, { useState, useEffect } from "react";
 
-function Feed() {
+const Feed = () => {
   const [feedData, setFeed] = useState({
     posts: [
       {
@@ -41,85 +41,168 @@ function Feed() {
       return;
     }
   };
-  const GenerateFeed = async () => {
-    for (let i = 0; i < feedData.posts.length; i++) {
-      <FeedTemplate
-        name={feedData.posts.name}
-        eventType={feedData.posts.eventType}
-        item={feedData.posts.item}
-        date={feedData.posts.date}
-      />;
-    }
-  };
-  const FeedTemplate = ({ name, eventType, item, date }) => (
-    <Card
-      style={{
-        fontSize: ".5rem",
-        backgroundColor: "#464646",
-        border: "solid #DADADA",
-        borderWidth: ".3em",
-        marginTop: ".1rem",
-        marginBottom: 0,
-        marginLeft: ".3rem",
-        marginRight: ".3rem",
-      }}
-    >
-      <Row xs="3">
-        <Col xs={1}>
-          <Image
-            src={Placeholder}
-            roundedCircle
-            align="left"
+
+  const FeedTemplate = (name, eventType, item, date) => {
+    let dateN = new Date(date);
+    return (
+      <Card
+        style={{
+          fontSize: ".5rem",
+          backgroundColor: "#464646",
+          border: "solid #DADADA",
+          borderWidth: ".3em",
+          marginTop: ".1rem",
+          marginBottom: 0,
+          marginLeft: ".3rem",
+          marginRight: ".3rem",
+        }}
+      >
+        <Row xs="3">
+          <Col xs={1}>
+            <Image
+              src={Placeholder}
+              roundedCircle
+              align="left"
+              style={{
+                width: "5em",
+                height: "5em",
+                margin: "1em",
+              }}
+              alt="ffff"
+            />
+          </Col>
+          <Col xs={6} style={{ marginTop: "auto", marginBottom: "auto" }}>
+            <label
+              style={{
+                fontSize: "2em",
+                color: "#DADADA",
+                marginLeft: "2em",
+                marginTop: "auto",
+                marginBottom: "auto",
+              }}
+            >
+              <strong>{name} </strong>
+              {eventType} {item}
+            </label>
+          </Col>
+          <Col
             style={{
-              width: "5em",
-              height: "5em",
-              margin: "1em",
-            }}
-            alt="ffff"
-          />
-        </Col>
-        <Col xs={6} style={{ marginTop: "auto", marginBottom: "auto" }}>
-          <label
-            style={{
-              fontSize: "2em",
-              color: "#DADADA",
-              marginLeft: "2em",
+              textAlign: "right",
               marginTop: "auto",
               marginBottom: "auto",
             }}
           >
-            <strong>{name} </strong>
-            {eventType} {item}
-          </label>
-        </Col>
-        <Col
-          style={{
-            textAlign: "right",
-            marginTop: "auto",
-            marginBottom: "auto",
-          }}
-        >
-          <label
-            style={{
-              fontSize: "1rem",
-              color: "#DADADA",
-              marginTop: "auto",
-              marginBottom: "auto",
-            }}
-          >
-            {date}
-          </label>
-        </Col>
-      </Row>
-      {/* <Card.Footer style={{ backgroundColor: "#464646", borderTopWidth: 0 }}>
+            <label
+              style={{
+                fontSize: "1rem",
+                color: "#DADADA",
+                marginTop: "auto",
+                marginBottom: "auto",
+              }}
+            >
+              {dateN}
+            </label>
+          </Col>
+        </Row>
+        {/* <Card.Footer style={{ backgroundColor: "#464646", borderTopWidth: 0 }}>
         <label style={{ fontSize: "1rem", color: "#DADADA", float: "right" }}>
           {timeStamp}
         </label>
       </Card.Footer> */}
-    </Card>
+      </Card>
+    );
+  };
+
+  let printFeed = (
+    <div>
+      {feedData.posts.map((p) => {
+        return (
+          <Card
+            style={{
+              fontSize: ".5rem",
+              backgroundColor: "#464646",
+              border: "solid #DADADA",
+              borderWidth: ".3em",
+              marginTop: ".1rem",
+              marginBottom: 0,
+              marginLeft: ".3rem",
+              marginRight: ".3rem",
+            }}
+          >
+            <Row xs="3">
+              <Col xs={1}>
+                <Image
+                  src={Placeholder}
+                  roundedCircle
+                  align="left"
+                  style={{
+                    width: "5em",
+                    height: "5em",
+                    margin: "1em",
+                  }}
+                  alt="ffff"
+                />
+              </Col>
+              <Col xs={6} style={{ marginTop: "auto", marginBottom: "auto" }}>
+                <label
+                  style={{
+                    fontSize: "2em",
+                    color: "#DADADA",
+                    marginLeft: "2em",
+                    marginTop: "auto",
+                    marginBottom: "auto",
+                  }}
+                >
+                  <strong>{p.name} </strong>
+                  {p.eventType} {p.item}
+                </label>
+              </Col>
+              <Col
+                style={{
+                  textAlign: "right",
+                  marginTop: "auto",
+                  marginBottom: "auto",
+                }}
+              >
+                <label
+                  style={{
+                    fontSize: "1rem",
+                    color: "#DADADA",
+                    marginTop: "auto",
+                    marginBottom: "auto",
+                  }}
+                >
+                  {p.date}
+                </label>
+              </Col>
+            </Row>
+          </Card>
+        );
+        // return (
+        //   <FeedTemplate
+        //     name={p.name}
+        //     eventType={p.eventType}
+        //     item={p.item}
+        //     date={p.date}
+        //   />
+        // );
+      })}
+    </div>
   );
 
-  return <div>Ok</div>;
-}
+  const GenerateFeed = async () => {
+    for (let i = 0; i < feedData.posts.length; i++) {
+      <FeedTemplate
+        name={feedData.posts.name[i]}
+        eventType={feedData.posts.eventType[i]}
+        item={feedData.posts.item[i]}
+        date={feedData.posts.date[i]}
+      />;
+    }
+  };
 
+  return <div>{printFeed}</div>;
+  // { printFeed };
+  // };
+};
 export default Feed;
