f25b594643b3a734b9a856754ffd31500d846ba1
StaceyD22 <stacey.dale2311@gmail.com>
2020-12-06 21:11:47 -0500

    add grid system

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 import UserNavBar from '../components/UserNavBar';
 import FoodTable from '../components/FoodTable';
+import Feed from '../components/Feed';
+import Row from 'react-bootstrap/Row';
 
 const DashboardPage = () =>
 {
@@ -9,9 +12,21 @@ const DashboardPage = () =>
     return(
       <div id="dashboardPage" className="backgroundPage fillBackground">
         <UserNavBar />
-        <div className="container-fluid">
-          <FoodTable />
-        </div>
+        <Container fluid>
+          <Row>
+            <Col xs={8}>
+              <FoodTable />
+            </Col>
+            <Col>
+              <div id="feedDiv">
+                <div id="feedWrapper">
+                  <h2 className="feedTitle">What's Cookin'?</h2>
+                  <Feed />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
 };
