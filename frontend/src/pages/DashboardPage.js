0f8fdd06b1f30286c0221771365463bd0fe3eaae
Jonathan Frucht <jonfrucht@gmail.com>
2020-12-09 15:05:26 -0500

    changed color of feed

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import UserNavBar from '../components/UserNavBar';
-import FoodTable from '../components/FoodTable';
-import Feed from '../components/Feed';
-import Row from 'react-bootstrap/Row';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import UserNavBar from "../components/UserNavBar";
+import FoodTable from "../components/FoodTable";
+import Feed from "../components/Feed";
+import Row from "react-bootstrap/Row";
 
-const DashboardPage = () =>
-{
-
-    return(
-      <div id="dashboardPage" className="backgroundPage fillBackground">
-        <UserNavBar />
-        <Container fluid>
-          <Row>
-            <Col xs={7}>
-                <FoodTable className="foodColumn"/>
-            </Col>
-            <Col>
-              <div id="feedDiv">
-                <div id="feedWrapper">
-                  <h2 className="feedTitle">What's Cookin'?</h2>
-                  <Feed />
-                </div>
+const DashboardPage = () => {
+  return (
+    <div id="dashboardPage" className="backgroundPage fillBackground">
+      <UserNavBar />
+      <Container fluid>
+        <Row>
+          <Col xs={7}>
+            <FoodTable className="foodColumn" />
+          </Col>
+          <Col>
+            <div id="feedDiv">
+              <div id="feedWrapper">
+                <h2 style={{ color: "#DADADA" }} className="feedTitle">
+                  What's Cookin'?
+                </h2>
+                <Feed />
               </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default DashboardPage;
