fa0e41b6c1217e7cd38884f65038bece130d993e
StaceyD22 <stacey.dale2311@gmail.com>
2020-12-08 20:37:33 -0500

    update salt

--- frontend/src/sha256.js
+++ frontend/src/sha256.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const sha256 = pwd => {
-    // pwd = pwd.concat(process.env.REACT_APP_SALT_SECRET);
+    pwd = pwd.concat('beepboop');
     function rightRotate(value, amount) {
         return (value>>>amount) | (value<<(32 - amount));
     };
